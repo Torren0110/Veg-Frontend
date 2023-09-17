42b8641ab3a6db1a575fc278b1bb9332cf61a787
Yashasvi Rana <yashasvi.0110@gmail.com>
2023-09-17 20:21:29 +0530

    made temporary changes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,168 +1,174 @@
-import React,{useContext, useEffect, useState} from "react";
-import { useFormik } from "formik";
-import "./Profile.css"
-import userService from "../../services/user-service";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { ShopContext } from "../../context/shop-context";
-import * as Yup from "yup";
+// import React,{useContext, useEffect, useState} from "react";
+// import { useFormik } from "formik";
+// import "./Profile.css"
+// import userService from "../../services/user-service";
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import { ShopContext } from "../../context/shop-context";
+// import * as Yup from "yup";
 
-const Profile = () => {
+// const Profile = () => {
 
-    const { uid, user }  = useContext(ShopContext)
-    const [edit, setEdit] = useState(false)
-    const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-    const toggleEdit = ()=>{
-      setEdit(!edit)
-    }
+//     const { uid, user }  = useContext(ShopContext)
+//     const [edit, setEdit] = useState(false)
+//     const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+//     const toggleEdit = ()=>{
+//       setEdit(!edit)
+//     }
 
     
-    const showToastMessage =(msg) => {
-      // console.log("called ",msg)
-      if(msg === "success"){
-        toast.success('UPDATED SUCCESSFULLY !', {
-          position: toast.POSITION.BOTTOM_CENTER,
-            autoClose: 1000,
-            pauseOnHover: false,
-            hideProgressBar: true,
-          });
-        }
-        else{
-          toast.warning(msg, {
-            position: toast.POSITION.BOTTOM_CENTER,
-            autoClose: 1000,
-            pauseOnHover: false,
-            hideProgressBar: true,
-          });
-        }
-      };
+//     const showToastMessage =(msg) => {
+//       // console.log("called ",msg)
+//       if(msg === "success"){
+//         toast.success('UPDATED SUCCESSFULLY !', {
+//           position: toast.POSITION.BOTTOM_CENTER,
+//             autoClose: 1000,
+//             pauseOnHover: false,
+//             hideProgressBar: true,
+//           });
+//         }
+//         else{
+//           toast.warning(msg, {
+//             position: toast.POSITION.BOTTOM_CENTER,
+//             autoClose: 1000,
+//             pauseOnHover: false,
+//             hideProgressBar: true,
+//           });
+//         }
+//       };
       
-      const { errors, touched, handleSubmit, getFieldProps } =
-      useFormik({
-        initialValues: {
-          username:user.username || "",
-          phone:user.phone || "",
-          email:user.email || "",
-          address:user.address || "",
-        },
-        enableReinitialize: true,
-        validationSchema: Yup.object({
-          username: Yup
-          .string()
-          .required('Username is required')
-          .min(5, 'Username must be at least 5 characters')
-          .max(20, 'Username must be at most 20 characters')
-          .matches(/^[a-zA-Z0-9_]+$/, 'Username can only contain letters, numbers, and underscores'),
-          phone: Yup.number()
-          .typeError("Phone must be a number")
-          .integer("Phone must be an integer")
-          .min(1000000000, "Phone number must have at least 10 digits")
-          .max(9999999999, "Phone number can have at most 10 digits")
-          .required("Please enter your phone"),
-          email: Yup.string().email().required("Please enter your email"),
-        }),
-        onSubmit:async(values) => {
-          // await delay(10000)
-          toggleEdit()
-          console.log("object passed: ",values);
-          userService.update(user._id,values)
-          .then((res) => {
-            console.log("success: ",res.data);
-            showToastMessage("success");
-          })
-          .catch((err) => {
-            console.log("err", err.response.data);
-            showToastMessage(err.response.data);
-          })
-        },
-      });
+//       const { errors, touched, handleSubmit, getFieldProps } =
+//       useFormik({
+//         initialValues: {
+//           username:user.username || "",
+//           phone:user.phone || "",
+//           email:user.email || "",
+//           address:user.address || "",
+//         },
+//         enableReinitialize: true,
+//         validationSchema: Yup.object({
+//           username: Yup
+//           .string()
+//           .required('Username is required')
+//           .min(5, 'Username must be at least 5 characters')
+//           .max(20, 'Username must be at most 20 characters')
+//           .matches(/^[a-zA-Z0-9_]+$/, 'Username can only contain letters, numbers, and underscores'),
+//           phone: Yup.number()
+//           .typeError("Phone must be a number")
+//           .integer("Phone must be an integer")
+//           .min(1000000000, "Phone number must have at least 10 digits")
+//           .max(9999999999, "Phone number can have at most 10 digits")
+//           .required("Please enter your phone"),
+//           email: Yup.string().email().required("Please enter your email"),
+//         }),
+//         onSubmit:async(values) => {
+//           // await delay(10000)
+//           toggleEdit()
+//           console.log("object passed: ",values);
+//           userService.update(user._id,values)
+//           .then((res) => {
+//             console.log("success: ",res.data);
+//             showToastMessage("success");
+//           })
+//           .catch((err) => {
+//             console.log("err", err.response.data);
+//             showToastMessage(err.response.data);
+//           })
+//         },
+//       });
       
-      if(uid === '') return <h1>Login First!!!</h1>
+//       if(uid === '') return <h1>Login First!!!</h1>
       
-      return (
-        <>
-    <ToastContainer/>
-    <div className="profile">
-    <div  className="maindiv1">
-            <div className="model-container">
-              <div className="model-left">
-                <h1 className="model-title">User Profile</h1>
+//       return (
+//         <>
+//     <ToastContainer/>
+//     <div className="profile">
+//     <div  className="maindiv1">
+//             <div className="model-container">
+//               <div className="model-left">
+//                 <h1 className="model-title">User Profile</h1>
                 
-                <form onSubmit={handleSubmit}>
-                  <div className="input-block">
-                    <label htmlFor="username" className="input-label">
-                      Username
-                    </label>
-                    <input
-                      name='username'
-                      {...getFieldProps('username')}
-                      readOnly={!edit}
-                    />
-                    {errors.username && touched.username ? (
-                      <p className="form-error">{errors.username}</p>
-                    ) : null}
-                  </div>
-                 <div className="input-block">
-                    <label htmlFor="phone" className="input-label">
-                      Phone
-                    </label>
-                    <input
-                      name="phone"
-                      {...getFieldProps('phone')}
-                      readOnly={!edit}
-                    />
-                    {errors.phone && touched.phone ? (
-                      <p className="form-error">{errors.phone}</p>
-                    ) : null}
-                  </div>
-                  <div className="input-block">
-                    <label htmlFor="email" className="input-label">
-                      Email
-                    </label>
-                    <input
-                      name="email"
-                      {...getFieldProps('email')}
-                      readOnly={!edit}
-                    />
-                    {errors.email && touched.email ? (
-                      <p className="form-error">{errors.email}</p>
-                    ) : null}
-                  </div>
-                  <div className="input-block">
-                    <label htmlFor="address" className="input-label">
-                      Address
-                    </label>
-                    <input
-                      name="address"
-                      {...getFieldProps('address')}
-                      readOnly={!edit}
-                    />
-                    {errors.address && touched.address ? (
-                      <p className="form-error">{errors.address}</p>
-                    ) : null}
-                  </div>
+//                 <form onSubmit={handleSubmit}>
+//                   <div className="input-block">
+//                     <label htmlFor="username" className="input-label">
+//                       Username
+//                     </label>
+//                     <input
+//                       name='username'
+//                       {...getFieldProps('username')}
+//                       readOnly={!edit}
+//                     />
+//                     {errors.username && touched.username ? (
+//                       <p className="form-error">{errors.username}</p>
+//                     ) : null}
+//                   </div>
+//                  <div className="input-block">
+//                     <label htmlFor="phone" className="input-label">
+//                       Phone
+//                     </label>
+//                     <input
+//                       name="phone"
+//                       {...getFieldProps('phone')}
+//                       readOnly={!edit}
+//                     />
+//                     {errors.phone && touched.phone ? (
+//                       <p className="form-error">{errors.phone}</p>
+//                     ) : null}
+//                   </div>
+//                   <div className="input-block">
+//                     <label htmlFor="email" className="input-label">
+//                       Email
+//                     </label>
+//                     <input
+//                       name="email"
+//                       {...getFieldProps('email')}
+//                       readOnly={!edit}
+//                     />
+//                     {errors.email && touched.email ? (
+//                       <p className="form-error">{errors.email}</p>
+//                     ) : null}
+//                   </div>
+//                   <div className="input-block">
+//                     <label htmlFor="address" className="input-label">
+//                       Address
+//                     </label>
+//                     <input
+//                       name="address"
+//                       {...getFieldProps('address')}
+//                       readOnly={!edit}
+//                     />
+//                     {errors.address && touched.address ? (
+//                       <p className="form-error">{errors.address}</p>
+//                     ) : null}
+//                   </div>
                   
-                  <div className="model-buttons">
-                    {
-                      edit
-                      ?
-                      <button type="button" className="input-button" onClick={handleSubmit} >
-                        Update
-                      </button>
-                      :
-                      <button type="button" className="input-button" onClick={toggleEdit}>
-                        Edit
-                      </button>
-                    }
-                  </div>
-                </form>
-              </div>
+//                   <div className="model-buttons">
+//                     {
+//                       edit
+//                       ?
+//                       <button type="button" className="input-button" onClick={handleSubmit} >
+//                         Update
+//                       </button>
+//                       :
+//                       <button type="button" className="input-button" onClick={toggleEdit}>
+//                         Edit
+//                       </button>
+//                     }
+//                   </div>
+//                 </form>
+//               </div>
              
-            </div>
-        </div>
-        </div>
-    </>
-  )
+//             </div>
+//         </div>
+//         </div>
+//     </>
+//   )
+// }
+
+// export default Profile
+
+const Profile = () => {
+  return <div>Not working</div>
 }
 
-export default Profile
+export default Profile;
--- src/components/SellForm/SellForm.jsx
+++ src/components/SellForm/SellForm.jsx
@@ -1,155 +1,161 @@
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import vegetableService from "../../services/vegetable-service";
-import { useState } from "react";
-import { Alert } from "@mui/material";
-import "./sellform.css";
-import { ShopContext } from "../../context/shop-context";
-import { useContext } from "react";
+// import { useForm } from "react-hook-form";
+// import { z } from "zod";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import vegetableService from "../../services/vegetable-service";
+// import { useState } from "react";
+// import { Alert } from "@mui/material";
+// import "./sellform.css";
+// import { ShopContext } from "../../context/shop-context";
+// import { useContext } from "react";
 
-const schema = z.object({
-  name: z.string().min(5).nonempty(),
-  price: z.number().positive(),
-  quantity: z.number().int().positive(),
-  image: z.custom((data) => {
-    if (
-      data.length === 0 ||
-      (data[0] instanceof File && data[0].type.startsWith("image/"))
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  }),
-});
+// const schema = z.object({
+//   name: z.string().min(5).nonempty(),
+//   price: z.number().positive(),
+//   quantity: z.number().int().positive(),
+//   image: z.custom((data) => {
+//     if (
+//       data.length === 0 ||
+//       (data[0] instanceof File && data[0].type.startsWith("image/"))
+//     ) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }),
+// });
+
+// const SellForm = () => {
+//   const { uid,user } = useContext(ShopContext);
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors, isValid },
+//     reset,
+//   } = useForm({ resolver: zodResolver(schema) });
+
+//   const [showAlert, setAlert] = useState(false);
+//   const [showError, setError] = useState(false);
+
+//   const onSubmit = (data) => {
+//     const formData = new FormData();
+//     formData.append("name", data.name);
+//     formData.append("price", data.price);
+//     formData.append("quantity", data.quantity);
+//     formData.append("image", data.image[0]);
+//     formData.append("uid", uid);
+
+//     vegetableService
+//       .create(formData)
+//       .then((res) => {
+//         setAlert(true);
+//       })
+//       .catch((err) => {
+//         setError(true);
+//       });
+//   };
+
+//   if(!user.seller){
+//     return <h1 className="noseller"> Not a seller account !!</h1>
+//   }
+
+//   return (
+//     <div className="form">
+//     <form
+//       className="sell-form"
+//       onSubmit={handleSubmit((data) => {
+//         onSubmit(data);
+//         reset();
+//       })}
+//     >
+//       {showAlert && (
+//         <Alert
+//           onClose={() => {
+//             setAlert(false);
+//           }}
+//         >
+//           Item Successfully Posted!!
+//         </Alert>
+//       )}
+
+//       {showError && (
+//         <Alert
+//           onClose={() => {
+//             setError(false);
+//           }}
+
+//           severity="error"
+//         >
+//           SignIn to sell an Item
+//         </Alert>
+//       )}
+//       <h2 className="item-heading">Sell Item</h2>
+//       <div className="mb-3">
+//         <label htmlFor="name" className="form-label">
+//           Name:
+//         </label>
+//         <input
+//           {...register("name")}
+//           type="text"
+//           id="name"
+//           className="form-control"
+//           autoComplete="off"
+//         />
+//         {errors.name && <p className="text-danger">{errors.name.message}</p>}
+//       </div>
+//       <div className="mb-3">
+//         <label htmlFor="price" className="form-label">
+//           Price:
+//         </label>
+//         <input
+//           {...register("price", { valueAsNumber: true })}
+//           type="number"
+//           id="price"
+//           className="form-control"
+//           autoComplete="off"
+//         />
+//         {errors.price && <p className="text-danger">{errors.price.message}</p>}
+//       </div>
+//       <div className="mb-3">
+//         <label htmlFor="quantity" className="form-label">
+//           Quantity:
+//         </label>
+//         <input
+//           {...register("quantity", { valueAsNumber: true })}
+//           type="number"
+//           id="quantity"
+//           className="form-control"
+//           autoComplete="off"
+//         />
+//         {errors.quantity && (
+//           <p className="text-danger">{errors.quantity.message}</p>
+//         )}
+//       </div>
+//       <div className="mb-3">
+//         <label htmlFor="image" className="form-label">
+//           Image:
+//         </label>
+//         <input
+//           {...register("image")}
+//           type="file"
+//           id="image"
+//           className="form-control"
+//           autoComplete="off"
+//         />
+//         {errors.image && <p className="text-danger">{errors.image.message}</p>}
+//       </div>
+
+//       <button className="btn btn-primary" type="submit">
+//         Submit
+//       </button>
+//     </form>
+//     </div>
+//   );
+// };
+
+// export default SellForm;
 
 const SellForm = () => {
-  const { uid,user } = useContext(ShopContext);
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-    reset,
-  } = useForm({ resolver: zodResolver(schema) });
-
-  const [showAlert, setAlert] = useState(false);
-  const [showError, setError] = useState(false);
-
-  const onSubmit = (data) => {
-    const formData = new FormData();
-    formData.append("name", data.name);
-    formData.append("price", data.price);
-    formData.append("quantity", data.quantity);
-    formData.append("image", data.image[0]);
-    formData.append("uid", uid);
-
-    vegetableService
-      .create(formData)
-      .then((res) => {
-        setAlert(true);
-      })
-      .catch((err) => {
-        setError(true);
-      });
-  };
-
-  if(!user.seller){
-    return <h1 className="noseller"> Not a seller account !!</h1>
-  }
-
-  return (
-    <div className="form">
-    <form
-      className="sell-form"
-      onSubmit={handleSubmit((data) => {
-        onSubmit(data);
-        reset();
-      })}
-    >
-      {showAlert && (
-        <Alert
-          onClose={() => {
-            setAlert(false);
-          }}
-        >
-          Item Successfully Posted!!
-        </Alert>
-      )}
-
-      {showError && (
-        <Alert
-          onClose={() => {
-            setError(false);
-          }}
-
-          severity="error"
-        >
-          SignIn to sell an Item
-        </Alert>
-      )}
-      <h2 className="item-heading">Sell Item</h2>
-      <div className="mb-3">
-        <label htmlFor="name" className="form-label">
-          Name:
-        </label>
-        <input
-          {...register("name")}
-          type="text"
-          id="name"
-          className="form-control"
-          autoComplete="off"
-        />
-        {errors.name && <p className="text-danger">{errors.name.message}</p>}
-      </div>
-      <div className="mb-3">
-        <label htmlFor="price" className="form-label">
-          Price:
-        </label>
-        <input
-          {...register("price", { valueAsNumber: true })}
-          type="number"
-          id="price"
-          className="form-control"
-          autoComplete="off"
-        />
-        {errors.price && <p className="text-danger">{errors.price.message}</p>}
-      </div>
-      <div className="mb-3">
-        <label htmlFor="quantity" className="form-label">
-          Quantity:
-        </label>
-        <input
-          {...register("quantity", { valueAsNumber: true })}
-          type="number"
-          id="quantity"
-          className="form-control"
-          autoComplete="off"
-        />
-        {errors.quantity && (
-          <p className="text-danger">{errors.quantity.message}</p>
-        )}
-      </div>
-      <div className="mb-3">
-        <label htmlFor="image" className="form-label">
-          Image:
-        </label>
-        <input
-          {...register("image")}
-          type="file"
-          id="image"
-          className="form-control"
-          autoComplete="off"
-        />
-        {errors.image && <p className="text-danger">{errors.image.message}</p>}
-      </div>
-
-      <button className="btn btn-primary" type="submit">
-        Submit
-      </button>
-    </form>
-    </div>
-  );
-};
+  return <div>Not working</div>
+}
 
 export default SellForm;
